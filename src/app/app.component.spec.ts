077005ea04b307ec63a2d7512f15f2b63a0d9a53
David Betancur Jaramillo <david.betancur@PRAGMA.COM.CO>
2019-09-05 09:20:56 -0500

    feature: i18n configuration, initial unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,43 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {HttpClient} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {async, TestBed} from '@angular/core/testing';
+import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
+import {AppComponent} from './app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
+// const TRANSLATIONS_ES = require('../assets/i18n/es.json');
+
 
 describe('AppComponent', () => {
+  // let translate: TranslateService;
+  // let http: HttpTestingController;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot({})
+      ],
+      providers: [
+        TranslateService
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ],
     }).compileComponents();
+    // translate = TestBed.get(TranslateService);
+    // http = TestBed.get(HttpTestingController);
   }));
 
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'clickam2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('clickam2');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('clickam2 app is running!');
-  });
 });
