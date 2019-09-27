937b729d84b700938590a1af7bf0604d6e3bbe9e
David Betancur Jaramillo <david.betancur@PRAGMA.COM.CO>
2019-09-27 15:17:28 -0500

    feature: integrate service activation account

--- src/app/modules/login/pages/login.component.spec.ts
+++ src/app/modules/login/pages/login.component.spec.ts
@@ -10,10 +10,33 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { Router } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserService } from 'src/app/services/user.service';
+import { of, throwError } from 'rxjs';
 
 describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  const mockUserService= jasmine.createSpyObj("UserService", ["activateProfile"]);
+
+  let data = {
+    state: 'Success',
+    email: 'david.betancur@pragma.com.co'
+  }
+
+  let dataError = {
+    state: 'Error',
+    email: 'david.betancur@pragma.com.co',
+    userMessage: 'Activación erronea'
+  }
+
+  let invalidRequest = {
+    state: {
+      error: 'Error'
+    },
+    error: 'Peticion invalida',
+    email: 'david.betancur@pragma.com.co'
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,10 +52,11 @@ describe("LoginComponent", () => {
         TranslateModule.forRoot({})
       ],
       providers: [
-        // Router
+        { provide: UserService, useValue: mockUserService }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
+    mockUserService.activateProfile.and.returnValue(of(data));
   }));
 
   beforeEach(() => {
@@ -48,5 +72,36 @@ describe("LoginComponent", () => {
   it("show register", () => {
     component.showRegister();
   });
+
+  describe('Error activation', () => {
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LoginComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      mockUserService.activateProfile.and.returnValue(of(dataError));
+    });
+
+    it('error activation', () => {
+      component.activateUser();
+      expect(mockUserService.activateProfile).toHaveBeenCalled();
+    });
+  });
+
+  describe('invalid request', () => {
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LoginComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      mockUserService.activateProfile.and.returnValue(throwError(invalidRequest));
+    });
+
+    it('invalid request', () => {
+      component.activateUser();
+      expect(mockUserService.activateProfile).toHaveBeenCalled();
+    });
+  });
+  
   
 });
