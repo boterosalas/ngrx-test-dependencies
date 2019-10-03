16b401260d4dbd475054b8cb3f3cac8af0bc84c6
david betancur jaramillo <david.betancur@pragma.com.co>
2019-10-03 18:42:03 -0500

    feature: header css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-
 export class AppComponent {
+  isHome: boolean;
+  internal: boolean;
 
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('es');
-    translate.use('es');
+  constructor(translate: TranslateService, private router: Router) {
+    translate.setDefaultLang("es");
+    translate.use("es");
+    const path = window.location.pathname;
+
+    router.events.subscribe((url: any) => {
+      if (path === "/") {
+        this.isHome = true;
+        this.internal = false
+      } else {
+        this.isHome = false;
+      }
+    });
   }
 }
