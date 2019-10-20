36a4fb66ecd3d55132ce9ae244da7a6a0a2b9e75
david betancur jaramillo <david.betancur@pragma.com.co>
2019-10-20 18:09:01 -0500

    feature: change api endpoints

--- src/environments/enviroment.qa.ts
+++ src/environments/enviroment.qa.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: false,
     URL_SECURITY: 'https://apitestexito.azure-api.net/QA-clickam-md-apisecurity/api/',
     URL_PROFILE: 'https://apitestexito.azure-api.net/QA-clickam-md-apiprofile/api/',
-    URL_CONTENT: 'https://apitestexito.azure-api.net/QA-clickam-md-apicontent/api/',
-    URL_REFERAL: 'https://apitestexito.azure-api.net/QA-clickam-md-apireferral/api/',
-    URL_REPORT: 'https://apitestexito.azure-api.net/QA-clickam-md-apicontent/api/',
+    URL_CONTENT: 'https://apitestexito.azure-api.net/QA-clickam-md-apicontent/api/product/',
+    URL_REFERAL: 'https://apitestexito.azure-api.net/QA-clickam-md-apireferral/api/Link/',
+    URL_REPORT: 'https://apitestexito.azure-api.net/QA-clickam-md-apicommission/api/Reports/',
     SUBSCRIPTION: 'f5edbcd2315e479aad33d80c58052fcc'
   };
   
