79cccad9e7fcb868feb8190ddab2123a17339818
David Betancur Jaramillo <david.betancur@pragma.com.co>
2019-10-28 10:37:28 -0500

    feature: add api end points pdn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  URL_SECURITY: 'https://apitestexito.azure-api.net/QA-clickam-md-apisecurity/api/Authentication/',
-  URL_PROFILE: 'https://apitestexito.azure-api.net/QA-clickam-md-apiprofile/api/userprofile/',
-  URL_CONTENT: 'https://apitestexito.azure-api.net/QA-clickam-md-apicontent/api/product/',
-  URL_REFERAL: 'https://apitestexito.azure-api.net/QA-clickam-md-apireferral/api/Link/',
-  URL_COMISSION: 'https://apitestexito.azure-api.net/QA-clickam-md-apicommission/api/',
+  URL_SECURITY: 'https://apimexito.azure-api.net/PD-clickam-md-apisecurity/api/Authentication/',
+  URL_PROFILE: 'https://apimexito.azure-api.net/PD-clickam-md-apiprofile/api/userprofile/',
+  URL_CONTENT: 'https://apimexito.azure-api.net/PD-clickam-md-apicontent/api/product/',
+  URL_REFERAL: 'https://apimexito.azure-api.net/PD-clickam-md-apireferral/api/Link/',
+  URL_COMISSION: 'https://apimexito.azure-api.net/PD-clickam-md-apicommission/api',
   SUBSCRIPTION: 'f5edbcd2315e479aad33d80c58052fcc'
 };
