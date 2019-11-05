ace636c3890d82965ca49e29bd3a64f16b7e346d
David Betancur Jaramillo <david.betancur@pragma.com.co>
2019-11-04 19:25:15 -0500

    feature: users html screen

--- src/app/modules/admin/components/dialog-user/dialog-user.component.spec.ts
+++ src/app/modules/admin/components/dialog-user/dialog-user.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogUserComponent } from './dialog-user.component';
+import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 describe('DialogUserComponent', () => {
   let component: DialogUserComponent;
   let fixture: ComponentFixture<DialogUserComponent>;
 
+  const dialogMock = {
+    close: () => { }
+   };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogUserComponent ]
+      declarations: [ DialogUserComponent ],
+      imports: [
+        AppMaterialModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        {provide: MatDialogRef, useValue: dialogMock},
+       ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
