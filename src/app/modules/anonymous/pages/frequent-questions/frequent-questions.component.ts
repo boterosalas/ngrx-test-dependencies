0f7d7cd43c0f62e7ed7a669ff18a514508705377
David Betancur Jaramillo <david.betancur@PRAGMA.COM.CO>
2019-12-06 19:13:21 -0500

    Feature: tag manager classes

--- src/app/modules/anonymous/pages/frequent-questions/frequent-questions.component.ts
+++ src/app/modules/anonymous/pages/frequent-questions/frequent-questions.component.ts
@@ -10,6 +10,15 @@ export class FrequentQuestionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.querySelector('.gtmPreguntaClicLatrlFunciona span').classList.add("gtmPreguntaClicLatrlFunciona");
+    document.querySelector('.grmPreguntaClicLatrlVenta span').classList.add("grmPreguntaClicLatrlVenta");
+    document.querySelector('.gtmPreguntaClicLatrlComisiones span').classList.add("gtmPreguntaClicLatrlComisiones");
+    document.querySelector('.gtmPreguntaClicLatrlOfertas span').classList.add("gtmPreguntaClicLatrlOfertas");
+    document.querySelector('.gtmPreguntaClicLatrlNegocios span').classList.add("gtmPreguntaClicLatrlNegocios");
+    document.querySelector('.gtmPreguntaClicLatrlFechasPago span').classList.add("gtmPreguntaClicLatrlFechasPago");
+    document.querySelector('.gtmPreguntaClicLatrlDondePagan span').classList.add("gtmPreguntaClicLatrlDondePagan");
+    document.querySelector('.gtmPreguntaClicLatrlMediosPagos span').classList.add("gtmPreguntaClicLatrlMediosPagos");
+    document.querySelector('.gtmPreguntaClicLatrlEnvios span').classList.add("gtmPreguntaClicLatrlEnvios");
   }
 
 }
