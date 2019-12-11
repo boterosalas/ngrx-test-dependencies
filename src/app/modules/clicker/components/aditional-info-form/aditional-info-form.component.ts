4e638cd701ece4e3e4201e48cc5c7966462be6fe
David Betancur Jaramillo <david.betancur@PRAGMA.COM.CO>
2019-12-11 18:30:03 -0500

    Feature: edit profile

--- src/app/modules/clicker/components/aditional-info-form/aditional-info-form.component.ts
+++ src/app/modules/clicker/components/aditional-info-form/aditional-info-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-aditional-info-form',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AditionalInfoFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  personalForm: FormGroup;
 
   ngOnInit() {
+  }
+
+  public personalFormInfo() {
+    this.personalForm = this.fb.group({
+      
+    })
   }
 
 }
