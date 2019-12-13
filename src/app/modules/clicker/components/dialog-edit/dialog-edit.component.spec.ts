3da523eb26a41b69f4cd8ddd21c2bf262df9248f
David Betancur Jaramillo <david.betancur@PRAGMA.COM.CO>
2019-12-12 20:37:02 -0500

    Feature: edit profile

--- src/app/modules/clicker/components/dialog-edit/dialog-edit.component.spec.ts
+++ src/app/modules/clicker/components/dialog-edit/dialog-edit.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogEditComponent } from './dialog-edit.component';
+import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 describe('DialogEditComponent', () => {
   let component: DialogEditComponent;
   let fixture: ComponentFixture<DialogEditComponent>;
 
+  const dialogMock = {
+    close: () => { }
+   };
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogEditComponent ]
+      declarations: [ DialogEditComponent ],
+      imports: [
+        AppMaterialModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        {provide: MatDialogRef, useValue: dialogMock},
+       ],
     })
     .compileComponents();
   }));
