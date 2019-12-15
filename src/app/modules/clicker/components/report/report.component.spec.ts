5df78e008f190a0e9618fecd2e3d8998e9d5c3f6
david betancur jaramillo <david.betancur@pragma.com.co>
2019-12-15 12:21:17 -0500

    feature: unit test report clicker

--- src/app/modules/clicker/components/report/report.component.spec.ts
+++ src/app/modules/clicker/components/report/report.component.spec.ts
@@ -1,40 +1,50 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ReportComponent } from './report.component';
-import { JwtModule } from '@auth0/angular-jwt';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
-import { TranslateModule } from '@ngx-translate/core';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { LinksService } from 'src/app/services/links.service';
-import { of } from 'rxjs/internal/observable/of';
+import { ReportComponent } from "./report.component";
+import { JwtModule } from "@auth0/angular-jwt";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { AppMaterialModule } from "src/app/modules/shared/app-material/app-material.module";
+import { TranslateModule } from "@ngx-translate/core";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { LinksService } from "src/app/services/links.service";
+import { of } from "rxjs/internal/observable/of";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('ReportComponent', () => {
+describe("ReportComponent", () => {
   let component: ReportComponent;
   let fixture: ComponentFixture<ReportComponent>;
 
   let mockLinksService = jasmine.createSpyObj("LinksService", [
     "getPayment",
+    "getInfomonth",
+    "getReports"
   ]);
 
-  const data = {
-    state: "Success",
-    userMessage: null,
-    objectResponse:
-      "https://webclickamdev.blob.core.windows.net/files-excel/ReportePagoComisiones20191021195441.xlsx"
+  const data = {users: [], total: 0}
+
+  let infoMonth = {
+    generalResume: {
+      totalCommissions: 0,
+      totalLinks: 0,
+      totalProducts: 0,
+      conversionRate: 0
+    },
+    money: { available: 0, account: 0 },
+    monthResume: { totalCommissions: 0, totalLink: 0, daysResume: [] }
   };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReportComponent ],
+      declarations: [ReportComponent],
       imports: [
         AppMaterialModule,
         TranslateModule.forRoot({}),
         HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
         JwtModule.forRoot({
           config: {
             tokenGetter: () => {
-              return localStorage.getItem('ACCESS_TOKEN');
+              return localStorage.getItem("ACCESS_TOKEN");
             },
             throwNoTokenError: true,
             whitelistedDomains: [],
@@ -43,27 +53,30 @@ describe('ReportComponent', () => {
         })
       ],
       providers: [{ provide: LinksService, useValue: mockLinksService }],
-      schemas: [
-        NO_ERRORS_SCHEMA
-      ]
-    })
-    .compileComponents();
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
     mockLinksService.getPayment.and.returnValue(of(data));
+    mockLinksService.getReports.and.returnValue(of(infoMonth));
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ReportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.identification= "123456789";
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('pagination', () => {
-    component.pagination({previousPageIndex: 1, pageIndex: 0, pageSize: 20, length: 5});
+  it("pagination", () => {
+    component.pagination({
+      previousPageIndex: 1,
+      pageIndex: 0,
+      pageSize: 20,
+      length: 5
+    });
     expect(mockLinksService.getPayment).toHaveBeenCalled();
   });
-
 });
