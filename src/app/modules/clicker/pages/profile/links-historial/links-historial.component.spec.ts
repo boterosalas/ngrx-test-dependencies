65f44e08ae5598567d9a7d5e0d11a1f3eae799f6
david betancur jaramillo <david.betancur@pragma.com.co>
2020-04-02 14:26:37 -0500

    Feature: unit test link historias

--- src/app/modules/clicker/pages/profile/links-historial/links-historial.component.spec.ts
+++ src/app/modules/clicker/pages/profile/links-historial/links-historial.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LinksHistorialComponent } from './links-historial.component';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LinksHistorialComponent', () => {
   let component: LinksHistorialComponent;
@@ -8,7 +10,11 @@ describe('LinksHistorialComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LinksHistorialComponent ]
+      declarations: [ LinksHistorialComponent ],
+      imports:[
+        SharedModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
