00a9d7120660bef8df7b89ecee421fc858fcbef4
david betancur jaramillo <david.betancur@pragma.com.co>
2020-05-31 11:56:46 -0500

    Feature: unit test slide video

--- src/app/modules/anonymous/components/slide-video/slide-video.component.spec.ts
+++ src/app/modules/anonymous/components/slide-video/slide-video.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SlideVideoComponent } from './slide-video.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
+import { ShareModule } from '@ngx-share/core';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+import { JwtModule } from '@auth0/angular-jwt';
 
 describe('SlideVideoComponent', () => {
   let component: SlideVideoComponent;
@@ -8,7 +16,26 @@ describe('SlideVideoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SlideVideoComponent ]
+      declarations: [ SlideVideoComponent ],
+      imports: [
+        SlickCarouselModule,
+        FlexLayoutModule,
+        AppMaterialModule,
+        ShareModule,
+        SharedModule,
+        BrowserAnimationsModule,
+        TranslateModule.forRoot(),
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: () => {
+              return localStorage.getItem("ACCESS_TOKEN");
+            },
+            throwNoTokenError: true,
+            whitelistedDomains: [],
+            blacklistedRoutes: []
+          }
+        })
+      ],
     })
     .compileComponents();
   }));
