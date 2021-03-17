740dd8fecd2cf81f0d6530c2008d139a7635556e
santiago.teran <santiago.teran@pragma.com.co>
2021-03-17 10:29:01 -0500

    corrections for QA

---
+++ src/app/modules/clicker/components/card-data-total/card-data-total.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardDataTotalComponent } from './card-data-total.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
+
+describe('CardDataComponent', () => {
+  let component: CardDataTotalComponent;
+  let fixture: ComponentFixture<CardDataTotalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CardDataTotalComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        AppMaterialModule
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardDataTotalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
