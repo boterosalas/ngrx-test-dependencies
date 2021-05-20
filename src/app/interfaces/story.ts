79a5e4c52e4d169a63a81e6cdbefa679cd347ee3
Diego Zapata <diego.zapata@pragma.com.co>
2021-05-20 08:59:56 -0500

    add interface story

---
+++ src/app/interfaces/story.ts
@@ -0,0 +1,14 @@
+export interface Story {
+    id: number,
+    idbusiness: number,
+    name: string,
+    businessName: string,
+    infoAditional: string,
+    image: string,
+    businessImage: string,
+    businessCode: string,
+    link: string,
+    date: any,
+    stateView: boolean,
+    pause: boolean
+}
