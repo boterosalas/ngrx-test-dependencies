682a8e749c33b1be175db379606f009a624b85d3
Freud <freud.munera@pragma.com.co>
2021-05-25 12:19:35 -0500

    Accordion links in footer

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { UtilsService } from 'src/app/services/utils.service';
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  panelOpenState = false;
 
   constructor(public auth: AuthService, private router: Router, private utils: UtilsService) {
 
