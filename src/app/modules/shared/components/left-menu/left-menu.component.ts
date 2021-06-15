e7f283575607d02732a9aa0777c2c7bb52cb1482
Freud <freud.munera@pragma.com.co>
2021-06-15 18:59:45 -0500

    sub items in menu

--- src/app/modules/shared/components/left-menu/left-menu.component.ts
+++ src/app/modules/shared/components/left-menu/left-menu.component.ts
@@ -1,43 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { animateText, onSideNavChange } from 'src/app/animations/animations';
-import { SidenavService } from 'src/app/services/sidenav.service';
+import { Component, OnInit } from "@angular/core";
+import { animateText, onSideNavChange } from "src/app/animations/animations";
+import { SidenavService } from "src/app/services/sidenav.service";
 
-
+interface children {
+  name: string;
+  link: string;
+}
 interface Page {
   link: string;
   name: string;
   icon: string;
+  childrens: children[];
 }
 
 @Component({
-  selector: 'app-left-menu',
-  templateUrl: './left-menu.component.html',
-  styleUrls: ['./left-menu.component.scss'],
-  animations: [onSideNavChange, animateText]
+  selector: "app-left-menu",
+  templateUrl: "./left-menu.component.html",
+  styleUrls: ["./left-menu.component.scss"],
+  animations: [onSideNavChange, animateText],
 })
 export class LeftMenuComponent implements OnInit {
-
   public sideNavState: boolean = false;
   public linkText: boolean = false;
 
   public pages: Page[] = [
-    {name: 'Inbox', link:'some-link', icon: 'inbox'},
-    {name: 'Starred', link:'some-link', icon: 'star'},
-    {name: 'Send email', link:'some-link', icon: 'send'},
-  ]
+    {
+      name: "Torre de control",
+      link: "www.google.com",
+      icon: "home",
+      childrens: [],
+    },
+    {
+      name: "Mercadeo",
+      link: "www.google.com",
+      icon: "leaderboard",
+      childrens: [{ name: "Herramientas", link: "http://google.com" }],
+    },
+    {
+      name: "Operación",
+      link: "www.google.com",
+      icon: "paid",
+      childrens: [{ name: "Herramientas2", link: "http://google.com" }],
+    },
+    {
+      name: "Configuración",
+      link: "www.google.com",
+      icon: "settings",
+      childrens: [{ name: "Herramientas3", link: "http://google.com" }],
+    },
+  ];
 
-  constructor(private _sidenavService: SidenavService) { }
+  constructor(private _sidenavService: SidenavService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSinenavToggle() {
-    this.sideNavState = !this.sideNavState
-    
+    this.sideNavState = !this.sideNavState;
+
     setTimeout(() => {
       this.linkText = this.sideNavState;
-    }, 200)
-    this._sidenavService.sideNavState$.next(this.sideNavState)
+    }, 200);
+    this._sidenavService.sideNavState$.next(this.sideNavState);
   }
-
 }
