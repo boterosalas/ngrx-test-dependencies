4ea866c49e49fd7a57fa50a601a9146d259f6812
David Betancur Jaramillo <david.betancur@pragma.com.co>
2021-07-06 19:04:45 -0500

    chip list

--- src/app/modules/admin/pages/users/dialog-filter-users/dialog-filter-users.component.ts
+++ src/app/modules/admin/pages/users/dialog-filter-users/dialog-filter-users.component.ts
@@ -1,24 +1,19 @@
-import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import * as moment from 'moment';
-import { Subscription } from 'rxjs';
-import { ContentService } from 'src/app/services/content.service';
+import { Component, Inject, Input, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import * as moment from "moment";
+import { Subscription } from "rxjs";
+import { ContentService } from "src/app/services/content.service";
 
 @Component({
-  selector: 'app-dialog-filter-users',
-  templateUrl: './dialog-filter-users.component.html',
-  styleUrls: ['./dialog-filter-users.component.scss']
+  selector: "app-dialog-filter-users",
+  templateUrl: "./dialog-filter-users.component.html",
+  styleUrls: ["./dialog-filter-users.component.scss"],
 })
 export class DialogFilterUsersComponent implements OnInit, OnDestroy {
+  constructor(private fb: FormBuilder, private content: ContentService) {}
 
-  constructor(
-    private fb: FormBuilder,
-    private content: ContentService
-    ) { 
-    }
-    
-    ngOnInit() {
+  ngOnInit() {
     this.filterForm();
     this.getAllBusiness();
   }
@@ -30,82 +25,86 @@ export class DialogFilterUsersComponent implements OnInit, OnDestroy {
   filterUsers: FormGroup;
 
   locale = {
-    locale: 'es',
-    direction: 'ltr', // could be rtl
-    weekLabel: 'W',
-    separator: ' a ', // default is ' - '
-    cancelLabel: 'Cancelar', // detault is 'Cancel'
-    applyLabel: 'Aplicar', // detault is 'Apply'
-    clearLabel: 'Limpiar', // detault is 'Clear'
-    customRangeLabel: 'Custom range',
+    locale: "es",
+    direction: "ltr", // could be rtl
+    weekLabel: "W",
+    separator: " a ", // default is ' - '
+    cancelLabel: "Cancelar", // detault is 'Cancel'
+    applyLabel: "Aplicar", // detault is 'Apply'
+    clearLabel: "Limpiar", // detault is 'Clear'
+    customRangeLabel: "Custom range",
     daysOfWeek: moment.weekdaysMin(),
     monthNames: moment.monthsShort(),
-    firstDay: 1 // first day is monday
-  }
+    firstDay: 1, // first day is monday
+  };
 
   bussiness = [];
   chipsBussiness = [];
 
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+
   status = [
-    {value: 'Registrados'},
-    {value: 'Activos nuevos'},
-    {value: 'Usuario de baja'},
-    {value: 'Activos'},
-    {value: 'Inactivos 1'},
-    {value: 'Inactivos 2'},
-    {value: 'Inactivos 3'},
+    { value: "Registrados" },
+    { value: "Activos nuevos" },
+    { value: "Usuario de baja" },
+    { value: "Activos" },
+    { value: "Inactivos 1" },
+    { value: "Inactivos 2" },
+    { value: "Inactivos 3" },
   ];
 
   comunication = [
-    {value: 'Recibe comunicación'},
-    {value: 'No recibe comunicación (Lista negra)'},
+    { value: "Recibe comunicación" },
+    { value: "No recibe comunicación (Lista negra)" },
   ];
 
   commissions = [
-    {value: 'Menos de $10.000'},
-    {value: 'Mas de $10.000'},
-    {value: 'Comisiones del periodo'},
+    { value: "Menos de $10.000" },
+    { value: "Mas de $10.000" },
+    { value: "Comisiones del periodo" },
   ];
 
-  accountBank = [
-    {value: 'Verificada'},
-    {value: 'No verificada'},
-  ];
+  accountBank = [{ value: "Verificada" }, { value: "No verificada" }];
 
-  documents =  [
-    {value: 'Con documentos'},
-    {value: 'Sin documentos'},
-  ];
-
+  documents = [{ value: "Con documentos" }, { value: "Sin documentos" }];
 
   public getAllBusiness() {
-    this.subscription = this.content.getAllBusiness().subscribe(resp => {
-     this.bussiness = resp;
-    })
-
+    this.subscription = this.content.getAllBusiness().subscribe((resp) => {
+      this.bussiness = resp;
+    });
   }
-
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
   public filterForm() {
-    this.filterUsers = this.fb.group(
-      {
-        dateRange: ['', Validators.required],
-        status: ['', Validators.required],
-        comunication: ['', Validators.required],
-        commissions: ['', Validators.required],
-        accountBank: ['', Validators.required],
-        bussiness: ['', Validators.required],
-        documents: ['', Validators.required],
-      }
-    );
+    this.filterUsers = this.fb.group({
+      dateRange: ["", Validators.required],
+      status: ["", Validators.required],
+      comunication: ["", Validators.required],
+      commissions: ["", Validators.required],
+      accountBank: ["", Validators.required],
+      bussiness: ["", Validators.required],
+      documents: ["", Validators.required],
+    });
   }
 
   public onChangeSelected(val) {
-    console.log(val)
+    if(this.chipsBussiness.length === 0) {
+      this.chipsBussiness.push(val);
+    } else {
+      if (this.chipsBussiness.includes(val) === false) this.chipsBussiness.push(val);
+    }
   }
 
+  remove(bussiness: any): void {
+    const index = this.chipsBussiness.indexOf(bussiness);
+
+    if (index >= 0) {
+      this.chipsBussiness.splice(index, 1);
+    }
+  }
 }
