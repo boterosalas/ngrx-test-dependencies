2b8469ee700280aa7d875404583b2793304328af
David Betancur Jaramillo <david.betancur@pragma.com.co>
2021-07-16 18:00:10 -0500

    unit test faqs

--- src/app/modules/admin/pages/help-center/help-center.component.spec.ts
+++ src/app/modules/admin/pages/help-center/help-center.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AdminModule } from '../../admin.module';
 
 import { HelpCenterComponent } from './help-center.component';
 
@@ -8,7 +10,11 @@ describe('HelpCenterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpCenterComponent ]
+      declarations: [],
+      imports: [
+        AdminModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
