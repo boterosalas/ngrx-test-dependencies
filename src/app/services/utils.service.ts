2495d4b716bc6c5fb94b60f26ca310313f7959ee
David Betancur Jaramillo <david.betancur@pragma.com.co>
2021-08-09 14:45:26 -0500

    fix styles and time picker edit

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -147,6 +147,6 @@ export class UtilsService {
     militaryTime = militaryTime.split(":");
     return militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2
       ? militaryTime[0] - 12 + ":" + militaryTime[1] + " PM"
-      : militaryTime.join(":") + " AM";
+      : militaryTime[0] + ':' + militaryTime[1]  + " AM";
   }
 }
