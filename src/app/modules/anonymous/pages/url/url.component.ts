1be0e4139089f67c67b615dfce1e8687ecaa34f8
Yeferson H <yeferson.hoyos@pragma.com.co>
2021-10-01 15:09:41 -0500

    se cambian textos y se agrega filtro

--- src/app/modules/anonymous/pages/url/url.component.ts
+++ src/app/modules/anonymous/pages/url/url.component.ts
@@ -4,17 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 import { AuthService } from 'src/app/services/auth.service';
 import { UtilsService } from 'src/app/services/utils.service';
-const widgetCodes = [
-  'exito_widget',
-  'carulla_widget',
-  'segurosexito_widget',
-  'movilexito_widget',
-  'beautyholics_widget',
-  'anutibara_widget',
-  'habi_widget',
-  'wesura_widget',
-  'haceb_widget',
-];
+
 @Component({
   selector: 'app-url',
   templateUrl: './url.component.html',
@@ -40,17 +30,7 @@ export class UrlComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.authSvc.isLoggedIn()) {
-      this.getUrl();
-    } else {
-      this.utilsSvc.showloginForm();
-      this.utilsSvc.change.subscribe((isOpen) => {
-        if (this.authSvc.isLoggedIn() && !isOpen) {
-          this.getUrl();
-        }
-      });
-    }
-
+    this.getUrl();
     this.metaTagService.addTags([
       {
         name: 'keywords',
@@ -65,22 +45,15 @@ export class UrlComponent implements OnInit {
   }
 
   public getUrl() {
-    let exist = widgetCodes.find((code) => code === this.code);
-
-
-    if (exist) {
-
-    } else {
-      this.link.getUrl(this.code).subscribe((url) => {
-        if (url !== null) {
-          window.location.replace(url);
-        }
-        if (url === null) {
-          this.showMessage = true;
-          this.router.navigate(['/']);
-          this.show = false;
-        }
-      });
-    }
+    this.link.getUrl(this.code).subscribe((url) => {
+      if (url !== null) {
+        window.location.replace(url);
+      }
+      if (url === null) {
+        this.showMessage = true;
+        this.router.navigate(['/']);
+        this.show = false;
+      }
+    });
   }
 }
