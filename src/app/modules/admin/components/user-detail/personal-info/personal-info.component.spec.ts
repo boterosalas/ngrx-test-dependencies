0c8999f1467211a770028b834287a9e4b4c6ee09
davidbetancur <david.betancur@pragma.com.co>
2021-10-19 17:29:42 -0500

    unit test

--- src/app/modules/admin/components/user-detail/personal-info/personal-info.component.spec.ts
+++ src/app/modules/admin/components/user-detail/personal-info/personal-info.component.spec.ts
@@ -2,10 +2,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { JwtModule } from '@auth0/angular-jwt';
 import { of } from 'rxjs/internal/observable/of';
 import { AppMaterialModule } from 'src/app/modules/shared/app-material/app-material.module';
+import { UserService } from 'src/app/services/user.service';
 
 import { PersonalInfoComponent } from './personal-info.component';
 
@@ -25,9 +27,17 @@ const data = {
     userId: 1
 }
 
+const response = {
+  state: 'Success',
+  userMessage: 'La informacion se ha actualizado',
+  objectResponse: true,
+};
+
 describe('PersonalInfoComponent', () => {
   let component: PersonalInfoComponent;
   let fixture: ComponentFixture<PersonalInfoComponent>;
+
+  const mockUserService = jasmine.createSpyObj('UserService', ['updateInfoClicker']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -37,6 +47,7 @@ describe('PersonalInfoComponent', () => {
         FormsModule,
         HttpClientTestingModule,
         RouterTestingModule,
+        BrowserAnimationsModule,
         AppMaterialModule,
         JwtModule.forRoot({
           config: {
@@ -52,6 +63,7 @@ describe('PersonalInfoComponent', () => {
       providers: [
         { provide: MatDialogRef, useValue: MatDialogMock },
         { provide: MAT_DIALOG_DATA, useValue: data },
+        { provide: UserService, useValue: mockUserService },
       ]
     })
     .compileComponents();
@@ -67,4 +79,24 @@ describe('PersonalInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('save info personal', () => {
+    mockUserService.updateInfoClicker.and.returnValue(of(response));
+    component.saveInfoPersonal();
+    expect(mockUserService.updateInfoClicker).toHaveBeenCalled();
+
+  });
+
+  it('save info personal error', () => {
+    mockUserService.updateInfoClicker.and.returnValue(of({}));
+    component.saveInfoPersonal();
+    expect(mockUserService.updateInfoClicker).toHaveBeenCalled();
+  });
+
+  it('edit info personal', () => {
+    component.editInfoPersonal();
+    expect(component.templatePersonalInfo).toBeDefined();
+  });
+
+
 });
