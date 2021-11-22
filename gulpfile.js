c2265cbaad2c571dc70840b134b30c5540d7e2ce
davidbetancur <david.betancur@pragma.com.co>
2021-11-22 09:50:24 -0500

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 var del = require('del');
+var rename = require("gulp-rename");
 
 gulp.task('compress', function () {
-  return gulp.src(['./dist/*.js']).pipe(gzip()).pipe(gulp.dest('./dist'));
+  return gulp.src(['./dist/**/*.js', './dist/**/*.map']).pipe(gzip()).pipe(gulp.dest('./dist'));
 });
 
 gulp.task('clean', function () {
-  return del(['./dist/*.js', '!./dist']);
+  return del(['./dist/**/*.js', './dist/**/*.map' ,'!./dist']);
 });
+
+gulp.task('change', function() {
+  return gulp.src(['./dist/**/*.gz'])
+      .pipe(rename(function (path) {
+        path.extname = path.extname.replace('.gz', '.js');
+        path.extname = path.extname.replace('.gz', '.map');
+      }))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('cleangz', function () {
+  return del(['./dist/**/*.gz', '!./dist']);
+});
+
+
