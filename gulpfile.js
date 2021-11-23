29b1b053ab1cf9c9022d39e8a343039aaa4d22ca
davidbetancur <david.betancur@pragma.com.co>
2021-11-23 18:35:42 -0500

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 // var gzip = require('gulp-gzip');
-var zlib = require('zlib');
-const gulpBrotli = require('gulp-brotli');
-var del = require('del');
-var rename = require("gulp-rename");
+// var zlib = require('zlib');
+// const gulpBrotli = require('gulp-brotli');
+// var del = require('del');
+// var rename = require("gulp-rename");
 var uglify = require('gulp-uglify-es').default;
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('uglify', function () {
   return gulp.src(['./dist/**/*.js'])
@@ -12,37 +14,49 @@ gulp.task('uglify', function () {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('minify-css', () => {
+  return gulp.src(['./dist/**/*.css'])
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify-html', () => {
+  return gulp.src('./dist/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'));
+});
+
 
 // gulp.task('compress', function () {
 //   return gulp.src(['./dist/**/*.js', './dist/**/*.map']).pipe(gzip()).pipe(gulp.dest('./dist'));
 // });
 
-gulp.task('compress', function () {
-  return gulp.src(['./dist/**/*.js', './dist/**/*.map']).pipe(gulpBrotli({
-    params: {
-      [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
-    },
-  })).pipe(gulp.dest('./dist'));
-});
+// gulp.task('compress', function () {
+//   return gulp.src(['./dist/**/*.js', './dist/**/*.map']).pipe(gulpBrotli({
+//     params: {
+//       [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+//     },
+//   })).pipe(gulp.dest('./dist'));
+// });
 
-gulp.task('clean', function () {
-  return del(['./dist/**/*.js', './dist/**/*.map' ,'!./dist']);
-});
+// gulp.task('clean', function () {
+//   return del(['./dist/**/*.js', './dist/**/*.map' ,'!./dist']);
+// });
 
-gulp.task('change', function() {
-  return gulp.src(['./dist/**/*.br'])
-      .pipe(rename(function (path) {
-        path.basename = path.basename.replace('.js', '');
-        path.basename = path.basename.replace('.js.map', '');
-        path.basename = path.basename.replace('.map', '');
-        path.extname = path.extname.replace('.br', '.js');
-      }))
-      .pipe(gulp.dest('./dist'));
-});
+// gulp.task('change', function() {
+//   return gulp.src(['./dist/**/*.br'])
+//       .pipe(rename(function (path) {
+//         path.basename = path.basename.replace('.js', '');
+//         path.basename = path.basename.replace('.js.map', '');
+//         path.basename = path.basename.replace('.map', '');
+//         path.extname = path.extname.replace('.br', '.js');
+//       }))
+//       .pipe(gulp.dest('./dist'));
+// });
 
-gulp.task('cleangz', function () {
-  return del(['./dist/**/*.br', '!./dist']);
-});
+// gulp.task('cleangz', function () {
+//   return del(['./dist/**/*.br', '!./dist']);
+// });
 
 
 
