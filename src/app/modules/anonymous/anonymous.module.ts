d77f893c09dbdc201d68cf9780d46b0f8b60da02
davidbetancur <david.betancur@pragma.com.co>
2022-01-13 15:11:21 -0500

    blog seo

--- src/app/modules/anonymous/anonymous.module.ts
+++ src/app/modules/anonymous/anonymous.module.ts
@@ -341,7 +341,7 @@ const routes: Routes = [
     component: TermsAndConditionsComponent,
   },
   {
-    path: 'tabla-comisiones',
+    path: 'tabla-recompensas',
     component: ComissionTableComponent,
   },
 ];
