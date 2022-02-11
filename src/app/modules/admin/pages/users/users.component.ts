44b87a832cbd7431e9d91574ce2bbfe3e7ad59d6
davidbetancur <david.betancur@pragma.com.co>
2022-02-11 10:24:38 -0500

    change state

--- src/app/modules/admin/pages/users/users.component.ts
+++ src/app/modules/admin/pages/users/users.component.ts
@@ -59,7 +59,7 @@ export class UsersComponent extends MatPaginatorIntl implements OnInit, OnDestro
       to: null,
       datestart: null,
       dateend: null,
-      state: null,
+      state: [],
       comunications: null,
       commissions: null,
       business: [],
