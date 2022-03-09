cd251a73d4d177b7dacb484518507be847abec14
davidbetancur <david.betancur@pragma.com.co>
2022-03-09 16:47:40 -0500

    unit test

--- src/app/modules/admin/components/catologue-card/catologue-card.component.spec.ts
+++ src/app/modules/admin/components/catologue-card/catologue-card.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
 
 import { CatologueCardComponent } from './catologue-card.component';
+
+export class MatDialogMock {
+  open() {
+    return {
+      afterClosed: () => of(true),
+    };
+  }
+}
 
 describe('CatologueCardComponent', () => {
   let component: CatologueCardComponent;
   let fixture: ComponentFixture<CatologueCardComponent>;
 
+  const matDialog = new MatDialogMock();
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CatologueCardComponent ]
+      declarations: [ CatologueCardComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: MatDialogMock },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: matDialog },
+      ],
     })
     .compileComponents();
   });
--- src/app/modules/admin/pages/catologue/catologue.component.spec.ts
+++ src/app/modules/admin/pages/catologue/catologue.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
 
 import { CatologueComponent } from './catologue.component';
+
+export class MatDialogMock {
+  open() {
+    return {
+      afterClosed: () => of(true),
+    };
+  }
+}
 
 describe('CatologueComponent', () => {
   let component: CatologueComponent;
   let fixture: ComponentFixture<CatologueComponent>;
 
+  const matDialog = new MatDialogMock();
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CatologueComponent ]
+      declarations: [ CatologueComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: MatDialogMock },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: matDialog },
+      ],
     })
     .compileComponents();
   });
