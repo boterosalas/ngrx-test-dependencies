a23f2b29eb52007330d7c840faa0789f61890285
davidbetancur <david.betancur@pragma.com.co>
2022-05-27 10:17:42 -0500

    tags

--- src/app/modules/anonymous/pages/comission-table/comission-table.component.ts
+++ src/app/modules/anonymous/pages/comission-table/comission-table.component.ts
@@ -28,23 +28,23 @@ export class ComissionTableComponent implements OnInit {
   addTagsTableComission() {
     if (isPlatformBrowser(this.platformId)) {
       this.intervalSubscription = this.interval.subscribe(() => {
-        document.querySelector('.mat-tab-label[aria-posinset="1"]').classList.add('gtmTablaComiClicMenuMovilExito');
-        document.querySelector('.mat-tab-label[aria-posinset="1"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuMovilExito');
+        document.querySelector('.mat-tab-label[aria-posinset="1"]').classList.add('gtmTablaDeRecompensasProductoMovilExito');
+        document.querySelector('.mat-tab-label[aria-posinset="1"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoMovilExito');
 
-        document.querySelector('.mat-tab-label[aria-posinset="2"]').classList.add('gtmTablaComiClicMenuSeguros');
-        document.querySelector('.mat-tab-label[aria-posinset="2"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuSeguros');
+        document.querySelector('.mat-tab-label[aria-posinset="2"]').classList.add('gtmTablaDeRecompensasProductoSeguros');
+        document.querySelector('.mat-tab-label[aria-posinset="2"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoSeguros');
 
-        document.querySelector('.mat-tab-label[aria-posinset="3"]').classList.add('gtmTablaComiClicMenuViajes');
-        document.querySelector('.mat-tab-label[aria-posinset="3"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuViajes');
+        document.querySelector('.mat-tab-label[aria-posinset="3"]').classList.add('gtmTablaDeRecompensasProductoViajes');
+        document.querySelector('.mat-tab-label[aria-posinset="3"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoViajes');
 
-        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaComiClicMenuWesura');
-        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuWesura');
+        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaDeRecompensasProductoWesura');
+        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoWesura');
 
-        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaComiClicMenuCarulla');
-        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuCarulla');
+        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaDeRecompensasProductoCarulla');
+        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoCarulla');
 
-        document.querySelector('.mat-tab-label[aria-posinset="5"]').classList.add('gtmTablaComiClicMenuExito');
-        document.querySelector('.mat-tab-label[aria-posinset="5"] .mat-tab-label-content').classList.add('gtmTablaComiClicMenuExito');
+        document.querySelector('.mat-tab-label[aria-posinset="5"]').classList.add('gtmTablaDeRecompensasProductoExito');
+        document.querySelector('.mat-tab-label[aria-posinset="5"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoExito');
       });
 
       if (document.querySelector('.mat-tab-label[aria-posinset="1"]')) {
