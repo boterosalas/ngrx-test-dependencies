7609cd988fac31cbdddf5d3dde5f44784cce6a50
davidbetancur <david.betancur@pragma.com.co>
2022-06-06 15:09:33 -0500

    tags

--- src/app/modules/anonymous/pages/comission-table/comission-table.component.ts
+++ src/app/modules/anonymous/pages/comission-table/comission-table.component.ts
@@ -16,7 +16,6 @@ export class ComissionTableComponent implements OnInit {
   intervalSubscription: Subscription;
   ngOnInit() {
     this.getComission();
-    this.addTagsTableComission();
   }
 
   public getComission() {
@@ -25,31 +24,5 @@ export class ComissionTableComponent implements OnInit {
     });
   }
 
-  addTagsTableComission() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.intervalSubscription = this.interval.subscribe(() => {
-        document.querySelector('.mat-tab-label[aria-posinset="1"]').classList.add('gtmTablaDeRecompensasProductoMovilExito');
-        document.querySelector('.mat-tab-label[aria-posinset="1"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoMovilExito');
 
-        document.querySelector('.mat-tab-label[aria-posinset="2"]').classList.add('gtmTablaDeRecompensasProductoSeguros');
-        document.querySelector('.mat-tab-label[aria-posinset="2"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoSeguros');
-
-        document.querySelector('.mat-tab-label[aria-posinset="3"]').classList.add('gtmTablaDeRecompensasProductoViajes');
-        document.querySelector('.mat-tab-label[aria-posinset="3"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoViajes');
-
-        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaDeRecompensasProductoWesura');
-        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoWesura');
-
-        document.querySelector('.mat-tab-label[aria-posinset="4"]').classList.add('gtmTablaDeRecompensasProductoCarulla');
-        document.querySelector('.mat-tab-label[aria-posinset="4"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoCarulla');
-
-        document.querySelector('.mat-tab-label[aria-posinset="5"]').classList.add('gtmTablaDeRecompensasProductoExito');
-        document.querySelector('.mat-tab-label[aria-posinset="5"] .mat-tab-label-content').classList.add('gtmTablaDeRecompensasProductoExito');
-      });
-
-      if (document.querySelector('.mat-tab-label[aria-posinset="1"]')) {
-        this.intervalSubscription.unsubscribe();
-      }
-    }
-  }
 }
