a151c785881cf10a8b5a1d6020aba2d8d507810c
davidbetancur <david.betancur@pragma.com.co>
2022-06-28 16:54:17 -0500

    buy

--- src/app/modules/shared/components/card-story/card-story.component.ts
+++ src/app/modules/shared/components/card-story/card-story.component.ts
@@ -589,7 +589,7 @@ export class CardStoryComponent implements OnInit, OnChanges, AfterViewInit {
     this.subscription = this.content.saveMission('BUY').subscribe();
     const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
     if (iOS) {
-      window.location.assign(this.urlshorten);
+      window.open(this.urlshorten, '_blank');
     } else {
       window.open(this.urlshorten, '_blank');
     }
