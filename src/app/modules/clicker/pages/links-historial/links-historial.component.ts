ad181259abfce4586b5240d979216fef13ae3e97
davidbetancur <david.betancur@pragma.com.co>
2022-07-06 09:21:18 -0500

    buy

--- src/app/modules/clicker/pages/links-historial/links-historial.component.ts
+++ src/app/modules/clicker/pages/links-historial/links-historial.component.ts
@@ -123,7 +123,6 @@ export class LinksHistorialComponent implements OnInit {
     const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
     if (iOS) {
       window.location.assign(this.urlshorten);
-      window.open(this.urlshorten, '_blank');
     } else {
       window.open(this.urlshorten, '_blank');
     }
