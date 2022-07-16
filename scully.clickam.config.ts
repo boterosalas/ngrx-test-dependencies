a28d5bab8f682a0dba7a5e3bcacae76c52131310
davidbetancur <david.betancur@pragma.com.co>
2022-07-15 20:57:22 -0500

    scully conf

--- scully.clickam.config.ts
+++ scully.clickam.config.ts
@@ -2,12 +2,26 @@ import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
 
-
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "clickam",
+  projectRoot: './src',
+  projectName: 'clickam',
   spsModulePath: 'YOUR OWN MODULE PATH HERE',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
+  extraRoutes: [
+    '/blog/que-es-clickam-y-como-ganar-dinero-online-con-la-plataforma',
+    '/blog/gana-10000-adicionales-a-tu-primera-recompensa',
+    '/blog/cupon-de-descuento-haceb',
+    '/blog/resuelve-todas-tus-dudas-sobre-los-dias-sin-iva',
+    '/blog/ahora-nunca-olvidaras-ganar-mientras-compras-nueva-extension-de-chrome',
+    '/blog/certificado-bancario-en-clickam',
+    '/blog/6-habilidades-de-un-vendedor-aplicadas-al-marketing-de-afiliados',
+    '/blog/que-es-marketing-de-afiliados',
+    '/blog/conoce-3-paginas-web-para-crear-logos-gratis',
+    '/blog/marketing-de-contenidos-aplicado-al-marketing-de-afiliados',
+    '/blog/seguros-exito-descuentos-y-beneficios',
+    '/blog/conoce-3-paginas-web-para-crear-el-nombre-de-tu-marca',
+    '/blog/eres-principiante-encuentra-las-mejores-herramientas',
+    '/blog/Bono-tennis-y-mic',
+  ]
 };
