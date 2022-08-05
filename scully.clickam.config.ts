55773015726e1844346fadac0f144cc1ea995b5a
davidbetancur <david.betancur@pragma.com.co>
2022-08-05 09:40:49 -0500

    scully

--- scully.clickam.config.ts
+++ scully.clickam.config.ts
@@ -1,9 +1,8 @@
 import { ScullyConfig } from '@scullyio/scully';
-const { fouc } =  require("@notiz/scully-plugin-fouc");
 const { lazyImages } =  require('@notiz/scully-plugin-lazy-images');
 const { MinifyHtml } = require('scully-plugin-minify-html');
 
-const postRenderers = [MinifyHtml, fouc, lazyImages];
+const postRenderers = [MinifyHtml, lazyImages];
 
 /** this loads the default render plugin, remove when switching to something else. */
 
