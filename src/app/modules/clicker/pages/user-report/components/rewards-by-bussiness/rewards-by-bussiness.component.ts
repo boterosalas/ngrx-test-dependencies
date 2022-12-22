b93eb5195eec7ebbf1338c0063110082a5fa24e8
Alessandro <alessandroRetuerto@hotmail.com>
2022-12-22 14:19:15 -0500

    feature/rewards-by-busasiness:Add card and styles

--- src/app/modules/clicker/pages/user-report/components/rewards-by-bussiness/rewards-by-bussiness.component.ts
+++ src/app/modules/clicker/pages/user-report/components/rewards-by-bussiness/rewards-by-bussiness.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rewards-by-bussiness',
   templateUrl: './rewards-by-bussiness.component.html',
-  styleUrls: ['./rewards-by-bussiness.component.scss']
+  styleUrls: ['./rewards-by-bussiness.component.scss'],
 })
 export class RewardsByBussinessComponent implements OnInit {
+  ojbRewards = [
+    {
+      img: 'https://webclickamqa.blob.core.windows.net/img-ofertas/pic-business/20220218113151.svg',
+      title: 'Almaecnes Éxito',
+      money: '23.000.000',
+      count: '12 productos',
+    },
+    {
+      img: 'https://webclickamqa.blob.core.windows.net/img-ofertas/pic-business/20220223110021.svg',
+      title: 'Almaecnes Éxito',
+      money: '23.000.000',
+      count: '12 productos',
+    },
+    {
+      img: 'https://webclickamqa.blob.core.windows.net/img-ofertas/pic-business/20220329163519.svg',
+      title: 'Almaecnes Éxito',
+      money: '23.000.000',
+      count: '12 productos',
+    },
+    {
+      img: 'https://webclickamqa.blob.core.windows.net/img-ofertas/pic-business/ico-seguros.svg',
+      title: 'Almaecnes Éxito',
+      money: '23.000.000',
+      count: '12 productos',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
