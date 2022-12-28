7a3994c1d00ea20ab7ed775b9973a8908f4a5274
david.boteros <david.boteros@pragma.com.co>
2022-12-28 01:54:05 -0500

    Estilos a la gráfica de recompensas

--- src/app/modules/clicker/pages/user-report/components/rewards-graph/rewards-graph.component.ts
+++ src/app/modules/clicker/pages/user-report/components/rewards-graph/rewards-graph.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
 
+const formatThousand = function (value: any) {
+  if (parseInt(value) >= 1000) {
+    return '$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  } else {
+    return '$' + value;
+  }
+}
+
 @Component({
   selector: 'app-rewards-graph',
   templateUrl: './rewards-graph.component.html',
@@ -22,28 +30,27 @@ export class RewardsGraphComponent implements OnInit {
 
       data: {// values on X-Axis
         labels: [
-          '2022-05-10',
-          '2022-05-11',
-          '2022-05-12',
-          '2022-05-13',
-          '2022-05-14',
-          '2022-05-15',
-          '2022-05-16',
-          '2022-05-17',
+          'Ene',
+          'Feb',
+          'Mar',
+          'Abr',
+          'May',
+          'Jun',
+          'Jul',
+          'Ago',
         ],
         datasets: [
           {
             data: [
-              '467',
+              '4670',
               '576',
               '572',
-              '79',
+              '79097',
               '92',
-              '574',
+              '574000',
               '573',
               '576'
             ],
-            // backgroundColor: 'blue',
             backgroundColor: 'transparent',
             pointBackgroundColor: '#FFFFFF',
             pointBorderColor: '#230040',
@@ -52,22 +59,56 @@ export class RewardsGraphComponent implements OnInit {
             pointRadius: 5,
             pointHoverRadius: 10,
             pointHoverBorderWidth: 5,
+            pointBorderWidth: 2,
             borderColor: '#8C80B4',
-            borderWidth: 2,
-            fill: true,
-            cubicInterpolationMode: 'monotone',
-
+            borderWidth: 4,
+            fill: false
           },
         ]
       },
       options: {
-        aspectRatio: 2.5,
+        responsive: true,
+        maintainAspectRatio: false,
+        interaction: {
+          intersect: false,
+          mode: 'nearest'
+        },
+        line: {
+          datasets: {
+            tension: 0,
+            borderWidth: 10
+          }
+        },
         plugins: {
           tooltip: {
             callbacks: {
-              title: () => '$ 10.000',
-              label: () => null
-            }
+              title: (items) => {
+                const item = items[0];
+                return formatThousand(item.formattedValue);
+              },
+              label: () => null,
+              labelTextColor: function () {
+                return '#FFFFFF';
+              }
+            },
+            titleFont: {
+              family: "galano-medium, sans-serif",
+              size: 14,
+              weight: '100'
+            },
+            bodyFont: {
+              family: "galano-medium, sans-serif"
+            },
+            backgroundColor: '#8C80B4',
+            padding: {
+              top: 12,
+              right: 14,
+              bottom: 6,
+              left: 14
+            },
+            yAlign: 'bottom',
+            xAlign: 'center',
+            titleAlign: 'center',
           },
           legend: {
             display: false
@@ -75,15 +116,47 @@ export class RewardsGraphComponent implements OnInit {
         },
         scales: {
           x: {
+            ticks: {
+              color: '#230040',
+              font: {
+                family: "galano-medium, sans-serif",
+                size: 14
+              },
+              padding: 20,
+            },
             border: {
-              display: false
+              display: false,
             },
             grid: {
               display: false,
-
             },
           },
           y: {
+            ticks: {
+              color: '#230040',
+              font: {
+                family: "galano-medium, sans-serif",
+                size: 14,
+              },
+              padding: 24,
+              callback: function nFormatter(num: number, digits) {
+                const lookup = [
+                  { value: 1, symbol: "" },
+                  { value: 1e3, symbol: "k" },
+                  { value: 1e6, symbol: "M" },
+                  { value: 1e9, symbol: "G" },
+                  { value: 1e12, symbol: "T" },
+                  { value: 1e15, symbol: "P" },
+                  { value: 1e18, symbol: "E" }
+                ];
+                const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+                var item = lookup.slice().reverse().find(function (item) {
+                  return num >= item.value;
+                });
+                return item ? `$ ${(num / item.value).toFixed(digits).replace(rx, "$1")}${item.symbol}` : "0";
+              }
+            },
+            beginAtZero: true,
             border: {
               display: false
             },
@@ -92,7 +165,7 @@ export class RewardsGraphComponent implements OnInit {
             }
           }
         },
-      }
+      },
 
     });
   }
