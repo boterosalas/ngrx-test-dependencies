7693a60f83dec9cc6efa4a5d4983281dd68edfef
Alessandro <alessandroRetuerto@hotmail.com>
2022-12-28 17:47:44 -0500

    addd tables and astyle and device

--- src/app/modules/clicker/pages/user-report/components/purchase-detail/purchase-detail.component.ts
+++ src/app/modules/clicker/pages/user-report/components/purchase-detail/purchase-detail.component.ts
@@ -1,31 +1,112 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { BreakpointService } from '../../../../../../services/breakpoint.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-purchase-detail',
   templateUrl: './purchase-detail.component.html',
   styleUrls: ['./purchase-detail.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*', minHeight: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class PurchaseDetailComponent implements OnInit {
-  constructor() {}
+export class PurchaseDetailComponent implements OnInit, OnDestroy {
+  breakpointService$ = new Subscription();
+  showInfoCard: boolean = false;
 
-  @Input() dataSource: any;
+  constructor(private breakpointService: BreakpointService) {}
 
-  displayedColumns: string[] = ['fecha', 'producto', 'cantidad', 'negocio', 'valordeVenta', 'recompensa', 'estado'];
-
-  ngOnInit() {}
-
-  validateStatus(status: string) {
-    const STATUS_COLOR = {
-      porvalidar: '#F4B062',
-      acumulado: '#5DB458',
-      rechazada: '#F25C5C',
-      porpagar: '#F27E5B',
-    };
-    const statusValue = this.convertString(status);
-    return STATUS_COLOR[statusValue];
+  ngOnInit() {
+    this.detectDevice();
   }
 
-  convertString(value: string) {
-    return value.toLowerCase().replace(' ', '');
+  dataSource = ELEMENT_DATA;
+
+  columnsStatus = [
+    {
+      label: 'fecha',
+      hideInMobile: false,
+    },
+    {
+      label: 'producto',
+      hideInMobile: true,
+    },
+    {
+      label: 'cantidad',
+      hideInMobile: true,
+    },
+    {
+      label: 'negocio',
+      hideInMobile: true,
+    },
+    {
+      label: 'valordeVenta',
+      hideInMobile: true,
+    },
+    {
+      label: 'recompensa',
+      hideInMobile: false,
+    },
+    {
+      label: 'estado',
+      hideInMobile: false,
+    },
+  ];
+
+  columnsToDisplay: any = this.columnsStatus.map((x) => x.label);
+
+  expandedElement: any | null;
+
+  detectDevice() {
+    this.breakpointService$ = this.breakpointService.isWidthLessThanBreakpoint('840').subscribe((data) => {
+      this.showInfoCard = data;
+    });
+  }
+  ngOnDestroy(): void {
+    this.breakpointService$.unsubscribe();
   }
 }
+
+const ELEMENT_DATA: any = [
+  {
+    fecha: '10/01/20',
+    producto: 'Camisa rosa',
+    cantidad: 1,
+    negocio: 'Almacenes Éxito',
+    valordeVenta: 132000,
+    recompensa: 13000,
+    estado: 'Por validar',
+  },
+  {
+    fecha: '10/01/20',
+    producto: 'Camisa rosa',
+    cantidad: 1,
+    negocio: 'Almacenes Éxito',
+    valordeVenta: 132000,
+    recompensa: 13000,
+    estado: 'Rechazada',
+  },
+  {
+    fecha: '10/01/20',
+    producto: 'Camisa rosa',
+    cantidad: 1,
+    negocio: 'Almacenes Éxito',
+    valordeVenta: 132000,
+    recompensa: 13000,
+    estado: 'Acumulado',
+  },
+  {
+    fecha: '10/01/20',
+    producto: 'Camisa rosa',
+    cantidad: 1,
+    negocio: 'Almacenes Éxito',
+    valordeVenta: 132000,
+    recompensa: 13000,
+    estado: 'Por pagar',
+  },
+];
