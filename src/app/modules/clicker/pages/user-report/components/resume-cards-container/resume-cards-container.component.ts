17d98edb975bf649fbe3e09808b577e493b59152
david.boteros <david.boteros@pragma.com.co>
2022-12-29 10:31:44 -0500

    Cambios de estilos

--- src/app/modules/clicker/pages/user-report/components/resume-cards-container/resume-cards-container.component.ts
+++ src/app/modules/clicker/pages/user-report/components/resume-cards-container/resume-cards-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { BreakpointService } from 'src/app/services/breakpoint.service';
 import { SwiperComponent } from "swiper/angular";
@@ -16,7 +16,16 @@ SwiperCore.use([FreeMode]);
   styleUrls: ['./resume-cards-container.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ResumeCardsContainerComponent implements OnInit, OnDestroy {
+export class ResumeCardsContainerComponent implements OnInit, OnDestroy, OnChanges {
+
+  isLoading: boolean = true;
+
+  @Input() dataSet: any = {
+    enValidacion: 0,
+    recompensas: 0,
+    pendientePorPago: 0,
+    rechazados: 0
+  };
 
   breakpoint$: Subscription = new Subscription();
   device: string = '';
@@ -27,26 +36,7 @@ export class ResumeCardsContainerComponent implements OnInit, OnDestroy {
     sticky: true
   };
 
-  resumeCards = [
-    {
-      class: 'rewards-card',
-      title: 'Recompensas del mes',
-      amount: 97502410,
-      tooltip: 'Este es el saldo que has generado durante el mes actual y está aprobado por el negocio aliado, se pagará a tu cuenta entre el 12 y 15 de este mes.'
-    },
-    {
-      class: 'outstanding-card',
-      title: 'Recompensa pendiente por pagar',
-      amount: 1250000,
-      tooltip: 'Estas son las recompensas que serán consignadas en tu cuenta entre el 15 y el 20 de este mes.'
-    },
-    {
-      class: 'rejected-card',
-      title: 'Recompensas rechazadas',
-      amount: 125000,
-      tooltip: 'Las recompensas que no se hicieron efectivas ya que la compra no fue efectuada de forma exitosa.'
-    },
-  ]
+  resumeCards = []
 
   constructor(
     private breakPointService: BreakpointService,
@@ -54,6 +44,34 @@ export class ResumeCardsContainerComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.breakpoint();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { dataSet } = changes;
+    if (dataSet.currentValue) {
+      this.isLoading = false;
+      this.dataSet = dataSet.currentValue;
+      this.resumeCards = [
+        {
+          class: 'rewards-card',
+          title: 'Recompensas del mes',
+          amount: this.dataSet.recompensas,
+          tooltip: 'Este es el saldo que has generado durante el mes actual y está aprobado por el negocio aliado, se pagará a tu cuenta entre el 12 y 15 de este mes.'
+        },
+        {
+          class: 'outstanding-card',
+          title: 'Recompensa pendiente por pagar',
+          amount: this.dataSet.pendientePorPago,
+          tooltip: 'Estas son las recompensas que serán consignadas en tu cuenta entre el 15 y el 20 de este mes.'
+        },
+        {
+          class: 'rejected-card',
+          title: 'Recompensas rechazadas',
+          amount: this.dataSet.rechazados,
+          tooltip: 'Las recompensas que no se hicieron efectivas ya que la compra no fue efectuada de forma exitosa.'
+        },
+      ]
+    }
   }
 
   breakpoint() {
--- src/app/modules/clicker/pages/user-report/user-report.component.ts
+++ src/app/modules/clicker/pages/user-report/user-report.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LinksService } from 'src/app/services/links.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-user-report',
   templateUrl: './user-report.component.html',
   styleUrls: ['./user-report.component.scss']
 })
-export class UserReportComponent implements OnInit {
+export class UserReportComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  getPayment$: Subscription = new Subscription();
+  getReportUser$: Subscription = new Subscription();
+  resumeCards: any;
+  userId: string;
+  pageTo: number = 20;
+
+  constructor(
+    private payment: LinksService,
+    private token: TokenService,
+  ) {
+    this.userId = this.token.user.userid;
+  }
 
   ngOnInit(): void {
+    this.getPayments();
+    this.getInfoMonth();
+  }
+
+  getPayments(from = 1, to = this.pageTo) {
+    const params = { from, to };
+    this.getPayment$ = this.payment.getPayment(this.userId, params).subscribe((payment: any) => {
+      console.log({ payment })
+    });
+  }
+
+  getInfoMonth() {
+    this.getReportUser$ = this.payment.getReportUser(this.userId).subscribe((resp: any) => {
+      console.log({ getReportUser: resp });
+      this.resumeCards = {
+        recompensas: parseInt(resp.objectResponse.money.cutOffValue) || 0,
+        enValidacion: parseInt(resp.objectResponse.money.validation) || 0,
+        pendientePorPago: parseInt(resp.objectResponse.money.accumulated) || 0,
+        rechazados: parseInt(resp.objectResponse.money.rejected) || 0
+      }
+      // this.totalAcumulated = resp.objectResponse.generalResume.totalCommissions;
+      // this.available = resp.objectResponse.money.accumulated;
+      // this.validation = resp.objectResponse.money.validation;
+      // this.account = resp.objectResponse.money.cutOffValue;
+      // this.rejected = resp.objectResponse.money.rejected || '0';
+      // this.conversionRate = resp.objectResponse.generalResume.conversionRate;
+      // this.totalLinks = resp.objectResponse.generalResume.totalLinks;
+      // this.totalProducts = resp.objectResponse.generalResume.totalProducts;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.getPayment$.unsubscribe();
+    this.getReportUser$.unsubscribe();
   }
 
 }
